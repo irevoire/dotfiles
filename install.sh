92afeee67d0744af0f14e78502dcf52ac091771c
irevoire <irevoire@hotmail.fr>
2020-05-29 12:54:11 +0200

    update the install script and add a gitignore global for rust and ocaml

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-cp ./config ../.config
-cp ./Xressources ../.Xressources
-cp ./zshrc ../.zshrc
+cp ./config ~/.config
+cp ./Xressources ~/.Xressources
+cp ./zshrc ~/.zshrc
+
+cp ./.gitignore_global ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
